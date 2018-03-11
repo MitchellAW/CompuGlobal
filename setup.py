12f54c5c6daca933d396aa76df932cfe66b1de66
Mitch Woollatt <mitchwoollatt@gmail.com>
2018-03-11 13:40:23 +1030

    Updated to latest version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 setup(
     name='compuglobal',
     packages=find_packages(exclude=['docs', 'doc', 'testing']),
-    version='0.1.2',
+    version='0.1.3',
     description='Unofficial python wrapper for the CGHMC API (Frinkiac, ' +
                 'Morbotron, Master Of All Science and more!)',
     author='MitchellAW',
@@ -12,7 +12,7 @@ setup(
     url='https://github.com/MitchellAW/CompuGlobal',
     # Where to dowload your package
     download_url='https://github.com/MitchellAW/CompuGlobal/' +
-                 'archive/0.1.2.tar.gz',
+                 'archive/0.1.3.tar.gz',
     keywords=['frinkiac', 'morbotron', 'master-of-all-science',
               'good-god-lemon', 'capital-beat-us', 'the-simpsons',
               'futurama', 'rick-and-morty', '30 rock', 'west wing', 'api'],
