d8e47ff93b6fde839ed356b5ea5b3c1ae35441b8
Mitch Woollatt <mitchwoollatt@gmail.com>
2018-03-11 13:38:31 +1030

    Updated download_url

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     url='https://github.com/MitchellAW/CompuGlobal',
     # Where to dowload your package
     download_url='https://github.com/MitchellAW/CompuGlobal/' +
-                 'archive/0.1.0.tar.gz',
+                 'archive/0.1.2.tar.gz',
     keywords=['frinkiac', 'morbotron', 'master-of-all-science',
               'good-god-lemon', 'capital-beat-us', 'the-simpsons',
               'futurama', 'rick-and-morty', '30 rock', 'west wing', 'api'],
