c6d9dcea2d7dec62c9e7ed0c6bce657f3e4b41a8
Mitch Woollatt <mitchwoollatt@gmail.com>
2018-03-12 13:46:21 +1030

    Added additional example files

---
+++ examples/all_shows.py
@@ -0,0 +1,23 @@
+import compuglobal
+
+# ----------------------------------------------------------------------------
+# This an example of the different APIs that are available for use:
+# ----------------------------------------------------------------------------
+
+# The Simpsons
+simpsons = compuglobal.Frinkiac()
+
+# Futurama
+futurama = compuglobal.Morbotron()
+
+# Rick and Morty
+rick_and_morty = compuglobal.MasterOfAllScience()
+
+# 30 Rock
+thirty_rock = compuglobal.GoodGodLemon()
+
+# West Wing
+west_wing = compuglobal.CapitalBeatUs()
+
+# The Simpsons' steamed hams skit
+steamed_hams = compuglobal.FrinkiHams()
