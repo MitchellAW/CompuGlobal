155e6c6f669ee96d0fcaa4cd92669b0bf176d7e1
Mitch Woollatt <mitchwoollatt@gmail.com>
2018-03-31 09:09:21 +1030

    Version bump to 0.2.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as f:
 setup(
     name='compuglobal',
     packages=find_packages(exclude=['docs', 'tests', 'examples']),
-    version='0.2.1',
+    version='0.2.2',
     description='Python wrapper for the CGHMC API (Frinkiac, Morbotron,' +
                 ' Master Of All Science and more!)',
     long_description=long_description,
