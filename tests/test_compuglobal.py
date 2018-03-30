bf614390762d44e6176272f48f6a7b61edce8fbf
Mitch Woollatt <mitchwoollatt@gmail.com>
2018-03-30 22:53:15 +1030

    Fixed tests for breaking change in latest update

--- tests/test_compuglobal.py
+++ tests/test_compuglobal.py
@@ -10,28 +10,47 @@ with open('frinkiac.json') as frinkiac:
     frinkiac_data = json.load(frinkiac)
 
 
+def frames_to_json(frames):
+    output = []
+    for frame in frames:
+        output.append(frame.json)
+
+    return output
+
+
 class CompuGlobalAPITestCase(unittest.TestCase):
     def test_json_to_caption_1(self):
         """Tests json converting to caption"""
-        self.assertEqual(FRINKIAC.json_to_caption(frinkiac_data['test_1']), '( yells) STUPID, SEXY FLANDERS! OW, MY LEGS! THIS IS THE WORST PAIN EVER... ')
-        self.assertEqual(FRINKIAC.json_to_caption(frinkiac_data['test_2']), 'Marge? Homer, I don\'t want to alarm you, ')
-        self.assertEqual(FRINKIAC.json_to_caption(frinkiac_data['test_3']), 'Moris:you die. ( screams) ')
+        self.assertEqual(FRINKIAC.json_to_caption(frinkiac_data['test_1']),
+                         '( yells) STUPID, SEXY FLANDERS! OW, MY LEGS! THIS IS '
+                         'THE WORST PAIN EVER... ')
+        self.assertEqual(FRINKIAC.json_to_caption(frinkiac_data['test_2']),
+                         'Marge? Homer, I don\'t want to alarm you, ')
+        self.assertEqual(FRINKIAC.json_to_caption(frinkiac_data['test_3']),
+                         'Moris:you die. ( screams) ')
 
     def test_view_episode_1(self):
         """Tests ``api/episode/{episode}/{start}/{end}`` endpoint"""
-        self.assertEqual(FRINKIAC.view_episode('S11E10', 339960, 0), frinkiac_data['episode_test_1'])
-        self.assertEqual(FRINKIAC.view_episode('S11E10', 339960, 349960), frinkiac_data['episode_test_2'])
+        self.assertEqual(FRINKIAC.view_episode('S11E10', 339960, 0),
+                         frinkiac_data['episode_test_1'])
+        self.assertEqual(FRINKIAC.view_episode('S11E10', 339960, 349960),
+                         frinkiac_data['episode_test_2'])
 
     def test_view_episode_2(self):
         """Tests ``api/episode/{episode}/{start}/{end}`` endpoint with
         invalid parameters"""
-        self.assertEqual(FRINKIAC.view_episode('Invalid', 0, 0), frinkiac_data['episode_test_3'])
+        self.assertEqual(FRINKIAC.view_episode('Invalid', 0, 0),
+                         frinkiac_data['episode_test_3'])
 
     def test_get_frames_1(self):
         """Tests ``api/frames/{episode}/{timestamp}/{before}/{after}``
         endpoint"""
-        self.assertEqual(FRINKIAC.get_frames('S02E01', 232973, 5000, 5000), frinkiac_data['frames_test_1'])
-        self.assertEqual(FRINKIAC.get_frames('S07E07', 225507, 3000, 3000), frinkiac_data['frames_test_2'])
+        self.assertEqual(frames_to_json(
+            FRINKIAC.get_frames('S02E01', 232973, 5000, 5000)),
+                         frinkiac_data['frames_test_1'])
+        self.assertEqual(frames_to_json(
+            FRINKIAC.get_frames('S07E07', 225507, 3000, 3000)),
+                         frinkiac_data['frames_test_2'])
 
     def test_get_frames_2(self):
         """Tests ``api/frames/{episode}/{timestamp}/{before}/{after}``
@@ -40,8 +59,12 @@ class CompuGlobalAPITestCase(unittest.TestCase):
 
     def test_get_nearby_frames_1(self):
         """Tests the ``api/nearby?e={}&t={}`` endpoint"""
-        self.assertEqual(FRINKIAC.get_nearby_frames('S07E10', 498797), frinkiac_data['nearby_frames_test_1'])
-        self.assertEqual(FRINKIAC.get_nearby_frames('S03E17', 1314891), frinkiac_data['nearby_frames_test_2'])
+        self.assertEqual(frames_to_json(
+            FRINKIAC.get_nearby_frames('S07E10', 498797)),
+                         frinkiac_data['nearby_frames_test_1'])
+        self.assertEqual(frames_to_json(
+            FRINKIAC.get_nearby_frames('S03E17', 1314891)),
+                         frinkiac_data['nearby_frames_test_2'])
 
     def test_get_nearby_frames_2(self):
         """Tests the ``api/nearby?e={}&t={}`` endpoint with invalid
@@ -51,8 +74,12 @@ class CompuGlobalAPITestCase(unittest.TestCase):
 
     def test_search_1(self):
         """Tests the ``api/search?q=`` endpoint"""
-        self.assertEqual(FRINKIAC.search('stupid sexy flanders'), frinkiac_data['search_test_1'])
-        self.assertEqual(FRINKIAC.search('i sleep in a racing car'), frinkiac_data['search_test_2'])
+        self.assertEqual(frames_to_json(
+            FRINKIAC.search('stupid sexy flanders')),
+            frinkiac_data['search_test_1'])
+        self.assertEqual(frames_to_json(
+            FRINKIAC.search('i sleep in a racing car')),
+            frinkiac_data['search_test_2'])
 
     def test_search_2(self):
         """Tests the ``api/search?q=`` endpoint with search query that
@@ -63,8 +90,10 @@ class CompuGlobalAPITestCase(unittest.TestCase):
     def test_search_for_screencap_1(self):
         """Tests the ``api/search?q=`` endpoint and checks it has the correct
         json data"""
-        self.assertEqual(FRINKIAC.search_for_screencap('stupid sexy flanders').json, frinkiac_data['test_1'])
-        self.assertEqual(FRINKIAC.search_for_screencap('i sleep in a racing car').json, frinkiac_data['search_test_3'])
+        self.assertEqual(FRINKIAC.search_for_screencap(
+            'stupid sexy flanders').json, frinkiac_data['test_1'])
+        self.assertEqual(FRINKIAC.search_for_screencap(
+            'i sleep in a racing car').json, frinkiac_data['search_test_3'])
 
     def test_search_for_screencap_2(self):
         """Tests the ``api/search?q=`` endpoint with search query that
--- tests/test_screencap.py
+++ tests/test_screencap.py
@@ -4,20 +4,19 @@ import unittest
 import compuglobal
 
 FRINKIAC = compuglobal.Frinkiac()
-MORBOTRON = compuglobal.Morbotron()
-MASTEROFALLSCIENCE = compuglobal.MasterOfAllScience()
+
+with open('frinkiac.json') as frinkiac:
+    frinkiac_data = json.load(frinkiac)
 
 
 class ScreencapTestCase(unittest.TestCase):
     """Tests for `screencap.py`."""
     def test_screencap_attributes(self):
         """Check attributes of Screencap"""
-        with open('frinkiac.json') as frinkiac:
-            test_data = json.load(frinkiac)
 
-        data = test_data['test_1']
+        data = frinkiac_data['test_1']
         screen = compuglobal.Screencap(FRINKIAC, data)
-        self.assertEqual(screen.id, 571)
+        self.assertEqual(screen.frame.id, 1503125)
         self.assertEqual(screen.key, 'S11E10')
         self.assertEqual(screen.episode, 10)
         self.assertEqual(screen.season, 11)
@@ -29,10 +28,10 @@ class ScreencapTestCase(unittest.TestCase):
                          'https://en.wikipedia.org/wiki/Little_Big_Mom')
         self.assertEqual(screen.timestamp, 339960)
         self.assertEqual(screen.caption,
-                         '( yells) STUPID, SEXY FLANDERS! OW, MY LEGS! THIS IS THE WORST PAIN EVER... ')
-        self.assertEqual(screen.image_url, 'https://frinkiac.com/img/{}/{}.jpg')
-        self.assertEqual(screen.meme_url,
-                         'https://frinkiac.com/meme/{}/{}.jpg?b64lines={}')
+                         '( yells) STUPID, SEXY FLANDERS! OW, MY LEGS! THIS '
+                         'IS THE WORST PAIN EVER... ')
+        self.assertEqual(screen.frame.image_url,
+                         'https://frinkiac.com/img/S11E10/339960.jpg')
         self.assertEqual(screen.gif_url,
                          'https://frinkiac.com/gif/{}/{}/{}.gif?b64lines={}')
         self.assertEqual(screen.mp4_url,
@@ -40,42 +39,40 @@ class ScreencapTestCase(unittest.TestCase):
 
     def test_simpsons_image_url(self):
         """Tests frinkiac image url"""
-        with open('frinkiac.json') as frinkiac:
-            test_data = json.load(frinkiac)
 
-        data = test_data['test_1']
+        data = frinkiac_data['test_1']
         screen = compuglobal.Screencap(FRINKIAC, data)
         self.assertEqual(screen.get_image_url(),
                          'https://frinkiac.com/img/S11E10/339960.jpg')
 
-        data = test_data['test_2']
+        data = frinkiac_data['test_2']
         screen = compuglobal.Screencap(FRINKIAC, data)
         self.assertEqual(screen.get_image_url(),
                          'https://frinkiac.com/img/S17E20/1082039.jpg')
 
-        data = test_data['test_3']
+        data = frinkiac_data['test_3']
         screen = compuglobal.Screencap(FRINKIAC, data)
         self.assertEqual(screen.get_image_url(),
                          'https://frinkiac.com/img/S07E06/530579.jpg')
 
     def test_simpsons_meme_url(self):
         """Tests frinkiac meme url"""
-        with open('frinkiac.json') as frinkiac:
-            test_data = json.load(frinkiac)
 
-        data = test_data['test_1']
+        data = frinkiac_data['test_1']
         screen = compuglobal.Screencap(FRINKIAC, data)
         self.assertEqual(screen.get_meme_url(),
                          'https://frinkiac.com/meme/S11E10/339960.jpg'
-                         '?b64lines=ICggeWVsbHMpIFNUVVBJRCwgU0VYWQogRkxBTkRFUlMhIE9XLCBNWSBMRUdTIQogVEhJUyBJUyBUSEUgV09SU1QgUEFJTgogRVZFUi4uLg==')
+                         '?b64lines=ICggeWVsbHMpIFNUVVBJRCwgU0VYWQogRkxBTkRFUl'
+                         'MhIE9XLCBNWSBMRUdTIQogVEhJUyBJUyBUSEUgV09SU1QgUEFJTg'
+                         'ogRVZFUi4uLg==')
 
-        data = test_data['test_2']
+        data = frinkiac_data['test_2']
         screen = compuglobal.Screencap(FRINKIAC, data)
         self.assertEqual(screen.get_meme_url('stupid sexy flanders'),
                          'https://frinkiac.com/meme/S17E20/1082039.jpg'
                          '?b64lines=IHN0dXBpZCBzZXh5IGZsYW5kZXJz')
 
-        data = test_data['test_3']
+        data = frinkiac_data['test_3']
         screen = compuglobal.Screencap(FRINKIAC, data)
         self.assertEqual(screen.get_meme_url(),
                          'https://frinkiac.com/meme/S07E06/530579.jpg'
@@ -83,132 +80,41 @@ class ScreencapTestCase(unittest.TestCase):
 
     def test_simpsons_get_gif_url(self):
         """Tests frinkiac gif urls"""
-        with open('frinkiac.json') as frinkiac:
-            test_data = json.load(frinkiac)
 
-        data = test_data['test_1']
+        data = frinkiac_data['test_1']
         screen = compuglobal.Screencap(FRINKIAC, data)
         self.assertEqual(screen.get_gif_url(),
                          'https://frinkiac.com/gif/S11E10/336960/343960.gif'
-                         '?b64lines=ICggeWVsbHMpIFNUVVBJRCwgU0VYWQogRkxBTkRFUlMhIE9XLCBNWSBMRUdTIQogVEhJUyBJUyBUSEUgV09SU1QgUEFJTgogRVZFUi4uLg==')
+                         '?b64lines=ICggeWVsbHMpIFNUVVBJRCwgU0VYWQogRkxBTkRFUl'
+                         'MhIE9XLCBNWSBMRUdTIQogVEhJUyBJUyBUSEUgV09SU1QgUEFJTg'
+                         'ogRVZFUi4uLg==')
         self.assertEqual(screen.get_gif_url(caption='Stupid Sexy Flanders'),
                          'https://frinkiac.com/gif/S11E10/336960/343960.gif'
                          '?b64lines=IFN0dXBpZCBTZXh5IEZsYW5kZXJz')
         self.assertEqual(screen.get_gif_url(before=1000, after=1000),
                          'https://frinkiac.com/gif/S11E10/338960/340960.gif'
-                         '?b64lines=ICggeWVsbHMpIFNUVVBJRCwgU0VYWQogRkxBTkRFUlMhIE9XLCBNWSBMRUdTIQogVEhJUyBJUyBUSEUgV09SU1QgUEFJTgogRVZFUi4uLg==')
+                         '?b64lines=ICggeWVsbHMpIFNUVVBJRCwgU0VYWQogRkxBTkRFUl'
+                         'MhIE9XLCBNWSBMRUdTIQogVEhJUyBJUyBUSEUgV09SU1QgUEFJTg'
+                         'ogRVZFUi4uLg==')
 
     def test_simpsons_get_mp4_url(self):
         """Tests frinkiac mp4 urls"""
-        with open('frinkiac.json') as frinkiac:
-            test_data = json.load(frinkiac)
 
-        data = test_data['test_1']
+        data = frinkiac_data['test_1']
         screen = compuglobal.Screencap(FRINKIAC, data)
         self.assertEqual(screen.get_mp4_url(),
                          'https://frinkiac.com/mp4/S11E10/336960/343960.mp4'
-                         '?b64lines=ICggeWVsbHMpIFNUVVBJRCwgU0VYWQogRkxBTkRFUlMhIE9XLCBNWSBMRUdTIQogVEhJUyBJUyBUSEUgV09SU1QgUEFJTgogRVZFUi4uLg==')
+                         '?b64lines=ICggeWVsbHMpIFNUVVBJRCwgU0VYWQogRkxBTkRFUlM'
+                         'hIE9XLCBNWSBMRUdTIQogVEhJUyBJUyBUSEUgV09SU1QgUEFJTgog'
+                         'RVZFUi4uLg==')
         self.assertEqual(screen.get_mp4_url(caption='Stupid Sexy Flanders'),
                          'https://frinkiac.com/mp4/S11E10/336960/343960.mp4'
                          '?b64lines=IFN0dXBpZCBTZXh5IEZsYW5kZXJz')
         self.assertEqual(screen.get_mp4_url(before=2500, after=500),
-                         'https://frinkiac.com/mp4/S11E10/337560/340360.mp4?b64lines=ICggeWVsbHMpIFNUVVBJRCwgU0VYWQogRkxBTkRFUlMhIE9XLCBNWSBMRUdTIQogVEhJUyBJUyBUSEUgV09SU1QgUEFJTgogRVZFUi4uLg==')
-
-    def test_futurama_meme_url(self):
-        """Tests morbotron meme url"""
-        with open('morbotron.json') as morbo:
-            test_data = json.load(morbo)
-
-        data = test_data['test_1']
-        screen = compuglobal.Screencap(MORBOTRON, data)
-        self.assertEqual(screen.get_meme_url(),
-                         'https://morbotron.com/meme/S07E03/343676.jpg?b64lines=IGFuZCB0aGUgcmVjZXB0aW9uCiBpc24ndCB2ZXJ5Li4uIFNodXQgdXAKIGFuZCB0YWtlIG15IG1vbmV5IQ==')
-
-        data = test_data['test_2']
-        screen = compuglobal.Screencap(MORBOTRON, data)
-        self.assertEqual(screen.get_meme_url('stupid sexy flanders'),
-                         'https://morbotron.com/meme/S05E09/1283064.jpg?b64lines=IHN0dXBpZCBzZXh5IGZsYW5kZXJz')
-
-        data = test_data['test_3']
-        screen = compuglobal.Screencap(MORBOTRON, data)
-        self.assertEqual(screen.get_meme_url(),
-                         'https://morbotron.com/meme/S04E08/696528.jpg?b64lines=IFRyZW1ibGUgaW4gZmVhciBhdCBvdXIKIHRocmVlIGRpZmZlcmVudCBraW5kcwogb2Ygc2hpcHMuIEFsbCByaWdodC4KIEl0J3MgU2F0dXJkYXkgbmlnaHQu')
-
-    def test_futurama_get_gif_url(self):
-        """Tests morbotron gif urls"""
-        with open('morbotron.json') as morbo:
-            test_data = json.load(morbo)
-
-        data = test_data['test_1']
-
-        screen = compuglobal.Screencap(MORBOTRON, data)
-
-        self.assertEqual(screen.get_gif_url(), 'https://morbotron.com/gif/S07E03/340757/347639.gif?b64lines=IGFuZCB0aGUgcmVjZXB0aW9uCiBpc24ndCB2ZXJ5Li4uIFNodXQgdXAKIGFuZCB0YWtlIG15IG1vbmV5IQ==')
-        self.assertEqual(screen.get_gif_url(caption='Stupid Sexy Flanders'),
-                         'https://morbotron.com/gif/S07E03/340757/347639.gif?b64lines=IFN0dXBpZCBTZXh5IEZsYW5kZXJz')
-        self.assertEqual(screen.get_gif_url(), 'https://morbotron.com/gif/S07E03/340757/347639.gif?b64lines=IGFuZCB0aGUgcmVjZXB0aW9uCiBpc24ndCB2ZXJ5Li4uIFNodXQgdXAKIGFuZCB0YWtlIG15IG1vbmV5IQ==')
-
-    def test_futurama_get_mp4_url(self):
-        """Tests morbotron mp4 urls"""
-        with open('morbotron.json') as morbo:
-            test_data = json.load(morbo)
-
-        data = test_data['test_1']
-        screen = compuglobal.Screencap(MORBOTRON, data)
-        self.assertEqual(screen.get_mp4_url(),
-                         'https://morbotron.com/mp4/S07E03/340757/347639.mp4?b64lines=IGFuZCB0aGUgcmVjZXB0aW9uCiBpc24ndCB2ZXJ5Li4uIFNodXQgdXAKIGFuZCB0YWtlIG15IG1vbmV5IQ==')
-        self.assertEqual(screen.get_mp4_url(caption='Stupid Sexy Flanders'),
-                         'https://morbotron.com/mp4/S07E03/340757/347639.mp4?b64lines=IFN0dXBpZCBTZXh5IEZsYW5kZXJz')
-        self.assertEqual(screen.get_mp4_url(before=2500, after=500),
-                         'https://morbotron.com/mp4/S07E03/341382/344093.mp4?b64lines=IGFuZCB0aGUgcmVjZXB0aW9uCiBpc24ndCB2ZXJ5Li4uIFNodXQgdXAKIGFuZCB0YWtlIG15IG1vbmV5IQ==')
-
-    def test_ram_meme_url(self):
-        """Tests master of all science meme url"""
-        with open('masterofall.json') as master:
-            test_data = json.load(master)
-
-        data = test_data['test_1']
-        screen = compuglobal.Screencap(MASTEROFALLSCIENCE, data)
-        self.assertEqual(screen.get_meme_url(),
-                         'https://masterofallscience.com/meme/S01E02/616991.jpg?b64lines=IFdoZXJlIGFyZSBteQogdGVzdGljbGVzLCBTdW1tZXI_CiBUaGV5IHdlcmUgcmVtb3ZlZC4KIFdoZXJlIGhhdmUgdGhleSBnb25lPw==')
-
-        data = test_data['test_2']
-        screen = compuglobal.Screencap(MASTEROFALLSCIENCE, data)
-        self.assertEqual(screen.get_meme_url('stupid sexy flanders'),
-                         'https://masterofallscience.com/meme/S02E09/1183599.jpg?b64lines=IHN0dXBpZCBzZXh5IGZsYW5kZXJz')
-
-        data = test_data['test_3']
-        screen = compuglobal.Screencap(MASTEROFALLSCIENCE, data)
-        self.assertEqual(screen.get_meme_url(),
-                         'https://masterofallscience.com/meme/S03E01/384050.jpg?b64lines=IFsgU3F1ZWFscyBdIFdoYXQ_')
-
-    def test_ram_get_gif_url(self):
-        """Tests master of all science gif urls"""
-        with open('masterofall.json') as master:
-            test_data = json.load(master)
-
-        data = test_data['test_1']
-        screen = compuglobal.Screencap(MASTEROFALLSCIENCE, data)
-        self.assertEqual(screen.get_gif_url(),
-                         'https://masterofallscience.com/gif/S01E02/614280/620954.gif?b64lines=IFdoZXJlIGFyZSBteQogdGVzdGljbGVzLCBTdW1tZXI_CiBUaGV5IHdlcmUgcmVtb3ZlZC4KIFdoZXJlIGhhdmUgdGhleSBnb25lPw==')
-        self.assertEqual(screen.get_gif_url(caption='Stupid Sexy Flanders'),
-                         'https://masterofallscience.com/gif/S01E02/614280/620954.gif?b64lines=IFN0dXBpZCBTZXh5IEZsYW5kZXJz')
-        self.assertEqual(screen.get_gif_url(before=1000, after=1000),
-                         'https://masterofallscience.com/gif/S01E02/616157/617826.gif?b64lines=IFdoZXJlIGFyZSBteQogdGVzdGljbGVzLCBTdW1tZXI_CiBUaGV5IHdlcmUgcmVtb3ZlZC4KIFdoZXJlIGhhdmUgdGhleSBnb25lPw==')
-
-    def test_ram_get_mp4_url(self):
-        """Tests master of all science mp4 urls"""
-        with open('masterofall.json') as master:
-            test_data = json.load(master)
-
-        data = test_data['test_1']
-        screen = compuglobal.Screencap(MASTEROFALLSCIENCE, data)
-        self.assertEqual(screen.get_mp4_url(),
-                         'https://masterofallscience.com/mp4/S01E02/614280/620954.mp4?b64lines=IFdoZXJlIGFyZSBteQogdGVzdGljbGVzLCBTdW1tZXI_CiBUaGV5IHdlcmUgcmVtb3ZlZC4KIFdoZXJlIGhhdmUgdGhleSBnb25lPw==')
-        self.assertEqual(screen.get_mp4_url(caption='Stupid Sexy Flanders'),
-                         'https://masterofallscience.com/mp4/S01E02/614280/620954.mp4?b64lines=IFN0dXBpZCBTZXh5IEZsYW5kZXJz')
-        self.assertEqual(screen.get_mp4_url(before=2500, after=500),
-                         'https://masterofallscience.com/mp4/S01E02/614697/617408.mp4?b64lines=IFdoZXJlIGFyZSBteQogdGVzdGljbGVzLCBTdW1tZXI_CiBUaGV5IHdlcmUgcmVtb3ZlZC4KIFdoZXJlIGhhdmUgdGhleSBnb25lPw==')
+                         'https://frinkiac.com/mp4/S11E10/337560/340360.mp4'
+                         '?b64lines=ICggeWVsbHMpIFNUVVBJRCwgU0VYWQogRkxBTkRFUlM'
+                         'hIE9XLCBNWSBMRUdTIQogVEhJUyBJUyBUSEUgV09SU1QgUEFJTgog'
+                         'RVZFUi4uLg==')
 
 
 if __name__ == '__main__':
