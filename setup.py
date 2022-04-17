50e8f9164d815cc6f7949e8dcf451c3a061f2b4c
Mitch Woollatt <MitchellAW@users.noreply.github.com>
2022-04-17 13:02:33 +0930

    Bumped aiohttp and python version requirements for project

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as f:
 setup(
     name='compuglobal',
     packages=find_packages(exclude=['docs', 'tests', 'examples']),
-    version='0.2.5',
+    version='0.2.6',
     description='Python wrapper for the CGHMC API (Frinkiac, Morbotron,' +
                 ' Master Of All Science and more!)',
     long_description=long_description,
@@ -21,7 +21,9 @@ setup(
     scripts=[],
     classifiers=['Intended Audience :: Developers',
                  'License :: OSI Approved :: MIT License',
-                 'Programming Language :: Python :: 3.5',
-                 'Programming Language :: Python :: 3.6'],
-    install_requires=['requests>=2.20.0', 'aiohttp>=3.6.0,<3.7.0']
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.8',
+                 'Programming Language :: Python :: 3.9',
+                 'Programming Language :: Python :: 3.10'],
+    install_requires=['requests>=2.20.0', 'aiohttp>=3.6.0,<4']
 )
