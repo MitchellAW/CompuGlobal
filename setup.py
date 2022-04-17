3cb70577f6bf0dadc089ed48823eb41a0ec78d4b
Mitch Woollatt <MitchellAW@users.noreply.github.com>
2022-04-17 13:21:19 +0930

    Added missing supported python version 3.7

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
     classifiers=['Intended Audience :: Developers',
                  'License :: OSI Approved :: MIT License',
                  'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
                  'Programming Language :: Python :: 3.8',
                  'Programming Language :: Python :: 3.9',
                  'Programming Language :: Python :: 3.10'],
