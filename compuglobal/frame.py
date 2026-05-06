3cfed1e8e00919bebc5cb4878e324a938c67dc81
Mitch Woollatt <21175347+MitchellAW@users.noreply.github.com>
2026-05-06 10:38:51 +0930

    Apply new formatting rules

--- compuglobal/frame.py
+++ compuglobal/frame.py
@@ -1,5 +1,5 @@
 class Frame:
-    """Represents a single frame of a TVShow/Movie/Skit generated using an 
+    """Represents a single frame of a TVShow/Movie/Skit generated using an
     instance of CompuGlobalAPI.
 
     Parameters
@@ -22,14 +22,14 @@ class Frame:
         image_url: str
             The direct url for the frame image.
     """
+
     def __init__(self, api, frame_json):
         self.json = frame_json
         self.api = api
-        self.id = frame_json['Id']
-        self.key = frame_json['Episode']
-        self.timestamp = frame_json['Timestamp']
-        self.image_url = api.URL + 'img/{}/{}.jpg'.format(self.key,
-                                                          self.timestamp)
+        self.id = frame_json["Id"]
+        self.key = frame_json["Episode"]
+        self.timestamp = frame_json["Timestamp"]
+        self.image_url = api.URL + "img/{}/{}.jpg".format(self.key, self.timestamp)
 
     def get_meme_url(self, caption=None):
         """Encodes the caption with base64 and then returns the meme url for
@@ -50,8 +50,7 @@ class Frame:
 
         b64_caption = self.api.encode_caption(caption)
 
-        return self.api.URL + 'meme/{}/{}.jpg?b64lines={}'.format(
-            self.key, self.timestamp, b64_caption)
+        return self.api.URL + "meme/{}/{}.jpg?b64lines={}".format(self.key, self.timestamp, b64_caption)
 
     def get_real_timestamp(self):
         """Gets a readable timestamp for the frame in format "mm:ss"
@@ -64,7 +63,7 @@ class Frame:
         seconds = int(self.timestamp / 1000)
         minutes = int(seconds / 60)
         seconds -= int(minutes * 60)
-        return '{}:{:02d}'.format(minutes, seconds)
+        return "{}:{:02d}".format(minutes, seconds)
 
     def __str__(self):
-        return self.api.title + ' - ' + self.key
+        return self.api.title + " - " + self.key
