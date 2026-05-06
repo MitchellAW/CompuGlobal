7fe72cc80924a2f8ce9541dbafbafe3c76ee9a6c
Mitch Woollatt <21175347+MitchellAW@users.noreply.github.com>
2026-05-06 10:47:51 +0930

    Add all package imports to __all__

--- compuglobal/__init__.py
+++ compuglobal/__init__.py
@@ -1,8 +1,20 @@
 from . import aio, api
+from .aio_screencap import AIOScreencap
+from .errors import APIPageStatusError, NoSearchResultsFound
+from .frame import Frame
+from .screencap import Screencap
 
 __title__ = "compuglobal"
 __author__ = "MitchellAW"
 __license__ = "MIT"
 __version__ = "0.2.7"
 
-__all__ = ["api", "aio"]
+__all__ = [
+    "api",
+    "aio",
+    "AIOScreencap",
+    "APIPageStatusError",
+    "NoSearchResultsFound",
+    "Frame",
+    "Screencap",
+]
