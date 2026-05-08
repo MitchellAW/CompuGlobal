50e0dca1082ec80a0171f8c8bd08b77aaad52952
Mitch Woollatt <21175347+MitchellAW@users.noreply.github.com>
2026-05-09 00:43:41 +0930

    Make all models importable from compuglobal

--- compuglobal/__init__.py
+++ compuglobal/__init__.py
@@ -1,9 +1,20 @@
 """Python wrapper for the CGHMC API (Frinkiac, Morbotron Master Of All Science and more!"""
 
-from .aio import CapitalBeatUs, Frinkiac, MasterOfAllScience, Morbotron
+from .aio import (
+    AsyncCompuGlobalAPI,
+    CapitalBeatUs,
+    Frinkiac,
+    MasterOfAllScience,
+    Morbotron,
+)
 from .errors import APIPageStatusError, NoSearchResultsFound
+from .models.comic import ComicStrip
+from .models.episode import Episode
+from .models.font import FontAlignment, FontColorRGB, FontFamily
 from .models.frame import Frame
 from .models.screencap import Screencap
+from .models.subtitle import Subtitle
+from .models.stream import StreamOverlay, Stream
 
 __title__ = "compuglobal"
 __author__ = "MitchellAW"
@@ -13,10 +24,19 @@ __version__ = "0.3.0"
 __all__ = [
     "APIPageStatusError",
     "NoSearchResultsFound",
+    "ComicStrip",
     "Frame",
     "Screencap",
+    "Episode",
+    "FontAlignment",
+    "FontColorRGB",
+    "FontFamily",
+    "Subtitle",
+    "Stream",
+    "StreamOverlay",
     "CapitalBeatUs",
     "Frinkiac",
     "MasterOfAllScience",
     "Morbotron",
+    "AsyncCompuGlobalAPI",
 ]
