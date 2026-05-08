ed08d3c9d3ad32828b0679ffc1a83e5bc274e2eb
Mitch Woollatt <21175347+MitchellAW@users.noreply.github.com>
2026-05-08 11:55:22 +0930

    Make async API classes available from package root

--- compuglobal/__init__.py
+++ compuglobal/__init__.py
@@ -1,4 +1,4 @@
-from . import aio, api
+from .aio import CapitalBeatUs, Frinkiac, MasterOfAllScience, Morbotron
 from .errors import APIPageStatusError, NoSearchResultsFound
 from .models.frame import Frame
 from .models.screencap import Screencap
@@ -9,10 +9,12 @@ __license__ = "MIT"
 __version__ = "0.2.7"
 
 __all__ = [
-    "api",
-    "aio",
     "APIPageStatusError",
     "NoSearchResultsFound",
     "Frame",
     "Screencap",
+    "CapitalBeatUs",
+    "Frinkiac",
+    "MasterOfAllScience",
+    "Morbotron",
 ]
