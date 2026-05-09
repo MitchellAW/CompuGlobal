5126fd63c6960dcf521818b8d396204fb253498a
Mitch Woollatt <21175347+MitchellAW@users.noreply.github.com>
2026-05-09 20:31:51 +0930

    Bump version number to 0.3.2

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "compuglobal"
 copyright = "2018, MitchellAW"
 author = "MitchellAW"
-release = "0.3.1"
+release = "0.3.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
