11270577c66bcbb70f2114ff9945f121892fd5ed
Mitch Woollatt <21175347+MitchellAW@users.noreply.github.com>
2026-05-18 21:44:13 +0930

    Implement pytest modules for api client and config

---
+++ tests/api/test_client.py
@@ -0,0 +1,86 @@
+"""Test API client module used for all API requests."""
+
+import aiohttp
+import pytest
+from aioresponses import aioresponses
+
+from compuglobal import APIPageStatusError
+from compuglobal.api.client import CompuGlobalAPIClient
+from compuglobal.api.endpoint import PreparedRequest, RequestMethod
+
+
+@pytest.mark.asyncio
+async def test_compuglobal_api_defaults() -> None:
+    async with aiohttp.ClientSession() as session:
+        client = CompuGlobalAPIClient(base_url="https://example.com", session=session)
+        assert client.base_url == "https://example.com"
+        assert client.session == session
+
+
+@pytest.mark.asyncio
+async def test_compuglobal_api_client_get(mock_aioresponse: aioresponses) -> None:
+    async with aiohttp.ClientSession() as session:
+        url = "https://example.com/test"
+        mock_aioresponse.get(url, payload={"ok": True})
+
+        client = CompuGlobalAPIClient(base_url="https://example.com", session=session)
+        resp = await client.get(url)
+        assert resp == {"ok": True}
+
+
+@pytest.mark.asyncio
+async def test_compuglobal_api_client_get_error(mock_aioresponse: aioresponses) -> None:
+    async with aiohttp.ClientSession() as session:
+        url = "https://example.com/test"
+        mock_aioresponse.get(url, status=400)
+
+        client = CompuGlobalAPIClient(base_url="https://example.com", session=session)
+        with pytest.raises(APIPageStatusError, match="Error 400"):
+            await client.get(url)
+
+
+@pytest.mark.asyncio
+async def test_compuglobal_api_client_post(mock_aioresponse: aioresponses) -> None:
+    async with aiohttp.ClientSession() as session:
+        url = "https://example.com/test"
+        mock_aioresponse.post(url, payload={"ok": True})
+
+        client = CompuGlobalAPIClient(base_url="https://example.com", session=session)
+        resp = await client.post_data(url=url, json={})
+        assert resp == '{"ok": true}'
+
+
+@pytest.mark.asyncio
+async def test_compuglobal_api_client_post_error(mock_aioresponse: aioresponses) -> None:
+    async with aiohttp.ClientSession() as session:
+        url = "https://example.com/test"
+        mock_aioresponse.post(url, status=500)
+
+        client = CompuGlobalAPIClient(base_url="https://example.com", session=session)
+        with pytest.raises(APIPageStatusError, match="Error 500"):
+            await client.post_data(url=url, json={})
+
+
+@pytest.mark.asyncio
+async def test_compuglobal_api_client_handle_request_get(mock_aioresponse: aioresponses) -> None:
+    async with aiohttp.ClientSession() as session:
+        url = "https://example.com/test"
+        mock_aioresponse.get(url, payload={"ok": True})
+
+        client = CompuGlobalAPIClient(base_url="https://example.com", session=session)
+
+        request = PreparedRequest(url=url, method=RequestMethod.GET)
+        resp = await client.handle_request(request)
+        assert resp == {"ok": True}
+
+
+@pytest.mark.asyncio
+async def test_compuglobal_api_client_handle_request_post(mock_aioresponse: aioresponses) -> None:
+    async with aiohttp.ClientSession() as session:
+        url = "https://example.com/test"
+        mock_aioresponse.post(url, payload={"ok": True})
+
+        client = CompuGlobalAPIClient(base_url="https://example.com", session=session)
+        request = PreparedRequest(url=url, method=RequestMethod.POST)
+        resp = await client.handle_request(request)
+        assert resp == '{"ok": true}'
