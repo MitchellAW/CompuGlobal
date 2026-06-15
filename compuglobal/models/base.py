a4173ac0a2afe09622653954c52b8c2e10802311
Mitch Woollatt <21175347+MitchellAW@users.noreply.github.com>
2026-06-15 20:51:18 +0930

    Add model config to docstring

--- compuglobal/models/base.py
+++ compuglobal/models/base.py
@@ -4,6 +4,22 @@ from pydantic import BaseModel, ConfigDict
 
 
 class BaseCompuGlobalModel(BaseModel, frozen=True):
-    """Base model class used for all API models."""
+    """Base model class used for all API models.
 
-    model_config = ConfigDict(extra="forbid", validate_by_name=True, validate_by_alias=True, serialize_by_alias=True)
+    .. code-block:: python
+
+        model_config = ConfigDict(
+            extra="forbid",
+            frozen=True,
+            serialize_by_alias=True,
+            validate_by_alias=True,
+            validate_by_name=True,
+        )
+    """
+
+    model_config = ConfigDict(
+        extra="forbid",
+        validate_by_name=True,
+        validate_by_alias=True,
+        serialize_by_alias=True,
+    )
